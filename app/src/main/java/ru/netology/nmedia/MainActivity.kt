31900e49ff01cddb71ba22451ebc8b75f83ad726
Rifmir Gatin <rifmir349@yandex.ru>
2021-10-19 13:05:47 +0300

    Добавил в проект viewBinding, чтобы не было хардкода по тексту

---
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -0,0 +1,16 @@
+package ru.netology.nmedia
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ru.netology.nmedia.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.textId.text = "NMedia!"
+    }
+}
