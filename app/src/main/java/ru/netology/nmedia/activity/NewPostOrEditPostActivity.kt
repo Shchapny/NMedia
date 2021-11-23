34613e7dbb5bd661d72c891a1107b3f3ffcb02c4
Rifmir Gatin <rifmir349@yandex.ru>
2021-11-24 00:18:55 +0300

    - рефакторинг кода, работа над отменой редактирования

--- app/src/main/java/ru/netology/nmedia/activity/NewPostOrEditPostActivity.kt
+++ app/src/main/java/ru/netology/nmedia/activity/NewPostOrEditPostActivity.kt
@@ -3,18 +3,32 @@ package ru.netology.nmedia.activity
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import ru.netology.nmedia.databinding.ActivityNewPostBinding
+import ru.netology.nmedia.util.AndroidUtils
+import ru.netology.nmedia.viewmodel.PostViewModel
 
 class NewPostOrEditPostActivity : AppCompatActivity() {
+
     lateinit var binding: ActivityNewPostBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivityNewPostBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-        val text = intent.getStringExtra(Intent.EXTRA_TEXT)
-        binding.edit.setText(text)
+        binding.edit.requestFocus()
+
+        val textEdit = intent.getStringExtra(Intent.EXTRA_TEXT)
+        if (textEdit != null) {
+            binding.boxEdit.visibility = View.VISIBLE
+            binding.textInfoEdit.text = textEdit
+        }
+        binding.edit.setText(textEdit)
+
+        val viewModel by viewModels<PostViewModel>()
 
         binding.ok.setOnClickListener {
             val intent = Intent()
@@ -24,8 +38,25 @@ class NewPostOrEditPostActivity : AppCompatActivity() {
             } else {
                 intent.putExtra(Intent.EXTRA_TEXT, text.toString())
                 setResult(Activity.RESULT_OK, intent)
+
+                viewModel.editContent(text.toString())
+                viewModel.add()
+
+                binding.edit.setText("")
+                binding.edit.clearFocus()
+                AndroidUtils.hideKeyboard(binding.edit)
+
             }
             finish()
         }
+
+        binding.close.setOnClickListener {
+            viewModel.add()
+            binding.edit.setText("")
+            binding.edit.clearFocus()
+            AndroidUtils.hideKeyboard(binding.edit)
+            finish()
+        }
+
     }
 }
