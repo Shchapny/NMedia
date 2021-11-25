fc26a52a41d1411cbb00418edc5eb0f055655c89
Rifmir Gatin <rifmir349@yandex.ru>
2021-11-25 14:48:16 +0300

    - добавил переменные, чтобы отображать дату и время публикации, но пока что работает коряво, отображает время когда было вновь созданно активити

--- app/src/main/java/ru/netology/nmedia/dto/Post.kt
+++ app/src/main/java/ru/netology/nmedia/dto/Post.kt
@@ -1,10 +1,13 @@
 package ru.netology.nmedia.dto
 
+import java.text.SimpleDateFormat
+import java.util.*
+
 data class Post(
     val id: Long = 0,
     val author: String = "Нетология. Университет интернет-профессий будущего",
     val content: String = "",
-    val published: String = "",
+    val published: String = formattedDate,
     val likedByMe: Boolean = false,
     val views: Long = 0,
     val likes: Long = 0,
@@ -12,4 +15,6 @@ data class Post(
     val video: String? = null
 )
 
-
+val date = Calendar.getInstance().time
+val formatter = SimpleDateFormat.getDateTimeInstance() //or use getDateInstance()
+val formattedDate = formatter.format(date)
